beffdb3fed6af700937079e9bdec173947f789b0
Tom MacWright <tom@macwright.org>
2014-06-03 13:51:53 -0400

    Fix putItem test bug

--- test/dynamo.js
+++ test/dynamo.js
@@ -1,83 +1,87 @@
 var test = require('tap').test,
-    dynamo = require('../lib/dynamodb');
+    dynamo = require('../lib/dynamodb'),
+    Dynalite = require('dynalite');
 
-function setup(cb) {
-    var dynalite = require('dynalite')({
+var dynalite;
+
+function setup(t) {
+    dynalite = Dynalite({
         createTableMs: 0,
         updateTableMs: 0,
         deleteTableMs: 0
     });
-    dynalite.listen(4567, cb.bind(cb, dynalite));
+    dynalite.listen(4567, function() {
+        t.end();
+    });
 }
 
+function teardown(t) {
+    dynalite.close();
+    t.end();
+}
+
+test('setup', setup);
 test('dynamo.createTable', function(t) {
-    setup(function(dynalite) {
-        dynamo.createTable(dynamo.client, function(err) {
-            t.notOk(err, 'no error');
-            t.pass('setup works');
-            dynalite.close();
-            t.end();
-        });
+    dynamo.createTable(dynamo.client, function(err) {
+        t.notOk(err, 'no error');
+        t.pass('table created');
+        t.end();
     });
 });
 
 test('dynamo.deleteTable', function(t) {
-    setup(function(dynalite) {
-        dynamo.createTable(dynamo.client, function(err) {
+    dynamo.createTable(dynamo.client, function(err) {
+        t.notOk(err, 'no error');
+        t.pass('setup works');
+        dynamo.deleteTable(dynamo.client, function(err) {
             t.notOk(err, 'no error');
-            t.pass('setup works');
-            dynamo.deleteTable(dynamo.client, function(err) {
-                t.notOk(err, 'no error');
-                t.pass('delete works');
-                dynalite.close();
-                t.end();
-            });
+            t.pass('deleteTable works');
+            t.end();
         });
     });
 });
+test('teardown', teardown);
 
+test('setup', setup);
 test('dynamo.putItem', function(t) {
-    setup(function(dynalite) {
-        dynamo.createTable(dynamo.client, created);
-        function created(err) {
-            t.notOk(err, 'no error');
-            dynamo.putItem(dynamo.client,
-                { id: 'a1', layer: 'b' }, 'geo',
-                inserted);
-        }
-        function inserted(err) {
-            t.notOk(err, 'no error');
-            t.pass('putItem');
-            dynalite.close();
-            t.end();
-        }
-    });
+    dynamo.createTable(dynamo.client, created);
+    function created(err) {
+        t.notOk(err, 'no error');
+        inserted();
+        dynamo.putItem(dynamo.client,
+            { id: 'a1', layer: 'b' },
+            inserted);
+    }
+    function inserted(err) {
+        t.notOk(err, 'no error');
+        t.pass('putItem');
+        t.end();
+    }
 });
+test('teardown', teardown);
 
-
+test('setup', setup);
 test('dynamo.getItem', function(t) {
     var item = { id: 'a2', layer: 'thisisalayername' };
-    setup(function(dynalite) {
-        dynamo.createTable(dynamo.client, created);
+    dynamo.createTable(dynamo.client, created);
 
-        function created(err) {
-            t.notOk(err, 'no error');
-            dynamo.putItem(dynamo.client,
-                item, 'geo',
-                inserted);
-        }
-        function inserted(err) {
-            t.notOk(err, 'no error');
-            t.pass('putItem');
-            dynamo.getItem(dynamo.client, item.layer, item.id, gotten);
-        }
-        function gotten(err, resp) {
-            t.notOk(err, 'no error');
-            t.deepEqual(resp, {
-                Item: dynamo._convertToDynamoTypes(item)
-            }, 'response');
-            dynalite.close();
-            t.end();
-        }
-    });
+    function created(err) {
+        t.notOk(err, 'no error');
+        dynamo.putItem(dynamo.client,
+            item,
+            inserted);
+    }
+    function inserted(err) {
+        t.notOk(err, 'no error');
+        t.pass('putItem');
+        dynamo.getItem(dynamo.client, item.layer, item.id, gotten);
+    }
+    function gotten(err, resp) {
+        t.notOk(err, 'no error');
+        t.deepEqual(resp, {
+            Item: dynamo._convertToDynamoTypes(item)
+        }, 'response');
+        t.end();
+    }
 });
+test('teardown', teardown);
