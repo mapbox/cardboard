a95a1a48c553b5c324fd154f04e529bbec988cc0
Mick Thompson <dthompson@gmail.com>
2015-12-15 17:25:50 -0500

    use much more basic bbox queries that we can page. revisit a better index plus paging in the future

--- index.js
+++ index.js
@@ -516,129 +516,51 @@ function Cardboard(config) {
      *   collection.type === 'FeatureCollection'; // true
      * });
      */
-    cardboard.bboxQuery = function(bbox, dataset, callback) {
-        var q = queue(100);
-
-        var bboxes = [bbox];
-        var epsilon = 1E-8;
-
-        // If a query crosses the (W) antimeridian/equator, we split it
-        // into separate queries to reduce overall throughput.
-        if (bbox[0] <= -180 && bbox[2] >= -180) {
-            bboxes = bboxes.reduce(function(memo, bbox) {
-                memo.push([bbox[0], bbox[1], -180 - epsilon, bbox[3]]);
-                memo.push([-180 + epsilon, bbox[1], bbox[2], bbox[3]]);
-                return memo;
-            }, []);
-
-            if (bbox[1] <= 0 && bbox[3] >= 0) {
-                bboxes = bboxes.reduce(function(memo, bbox) {
-                    memo.push([bbox[0], bbox[1], bbox[2], -epsilon]);
-                    memo.push([bbox[0], epsilon, bbox[2], bbox[3]]);
-                    return memo;
-                }, []);
-            }
+    cardboard.bboxQuery = function(bbox, dataset, options, callback) {
+        if (typeof options === 'function') {
+            callback = options;
+            options = {};
         }
 
-        // Likewise, if a query crosses the (E) antimeridian/equator,
-        // we split it.
-        else if (bbox[0] <= 180 && bbox[2] >= 180) {
-            bboxes = bboxes.reduce(function(memo, bbox) {
-                memo.push([bbox[0], bbox[1], 180 - epsilon, bbox[3]]);
-                memo.push([180 + epsilon, bbox[1], bbox[2], bbox[3]]);
-                return memo;
-            }, []);
+        options = _.defaults(options, { limit: 100 });
 
-            if (bbox[1] <= 0 && bbox[3] >= 0) {
-                bboxes = bboxes.reduce(function(memo, bbox) {
-                    memo.push([bbox[0], bbox[1], bbox[2], -epsilon]);
-                    memo.push([bbox[0], epsilon, bbox[2], bbox[3]]);
-                    return memo;
-                }, []);
+        // First find features indexed in children of this tile
+        var query = {
+            dataset: { EQ: dataset }
+        };
+
+        var queryOptions = {
+            pages: 1,
+            limit: options.limit,
+            filter: {
+                west: { LE: bbox[2] },
+                east: { GE: bbox[0] },
+                north: { GE: bbox[1] },
+                south: { LE: bbox[3] }
             }
-        }
+        };
 
-        // If a query crosses the equator/prime meridian, we split it.
-        else if (bbox[0] <= 0 && bbox[2] >= 0) {
-            bboxes = bboxes.reduce(function(memo, bbox) {
-                memo.push([bbox[0], bbox[1], -epsilon, bbox[3]]);
-                memo.push([epsilon, bbox[1], bbox[2], bbox[3]]);
-                return memo;
-            }, []);
+        var maxPages = 10;
+        var page = 0;
+        var combinedFeatures = [];
 
-            if (bbox[1] <= 0 && bbox[3] >= 0) {
-                bboxes = bboxes.reduce(function(memo, bbox) {
-                    memo.push([bbox[0], bbox[1], bbox[2], -epsilon]);
-                    memo.push([bbox[0], epsilon, bbox[2], bbox[3]]);
-                    return memo;
-                }, []);
-            }
-        }
-
-        var tiles = bboxes.map(function(bbox) {
-            return tilebelt.bboxToTile(bbox);
-        });
-
-        // Deduplicate subquery tiles.
-        uniq(tiles, function(a, b) {
-            return !tilebelt.tilesEqual(a, b);
-        });
-
-        if (tiles.length > 1) {
-            // Filter out the z0 tile -- we'll always search it eventually.
-            tiles = _.filter(tiles, function(item) {
-                return item[2] !== 0;
-            });
-        }
-
-        tiles.forEach(function(tile) {
-            var tileKey = tilebelt.tileToQuadkey(tile);
-
-            // First find features indexed in children of this tile
-            var query = {
-                cell: { BEGINS_WITH: 'cell!' + tileKey },
-                dataset: { EQ: dataset }
-            };
-
-            var options = {
-                pages: 0,
-                index: 'cell',
-                filter: {
-                    west: { LE: bbox[2] },
-                    east: { GE: bbox[0] },
-                    north: { GE: bbox[1] },
-                    south: { LE: bbox[3] }
-                }
-            };
-            q.defer(config.dyno.query, query, options);
-
-            // Travel up the parent tiles, finding features indexed in each
-            var parentTileKey = tileKey.slice(0, -1);
-
-            while (tileKey.length > 0) {
-                query.cell = { EQ: 'cell!' + parentTileKey };
-                q.defer(config.dyno.query, query, options);
-                if (parentTileKey.length === 0) break;
-                parentTileKey = parentTileKey.slice(0, -1);
-            }
-        });
-
-        q.awaitAll(function(err, items) {
-            if (err) return callback(err);
-
-            items = _.flatten(items);
-
-            // Reduce the response's records to the set of
-            // records with unique ids.
-            uniq(items, function(a, b) {
-                return a.id !== b.id;
-            });
-
-            utils.resolveFeatures(items, function(err, data) {
+        function getPageOfBbox() {
+            config.dyno.query(query, queryOptions, function(err, items, meta) {
                 if (err) return callback(err);
-                callback(err, data);
+                utils.resolveFeatures(items, function(err, data) {
+                    if (err) return callback(err);
+                    combinedFeatures = combinedFeatures.concat(data.features);
+                    if (combinedFeatures.length >= options.limit || page >= maxPages || !meta[0].last) {
+                        data.features = combinedFeatures.slice(0,options.limit);
+                        return callback(err, data);
+                    }
+                    page +=1;
+                    queryOptions.start = meta[0].last;
+                    getPageOfBbox();
+                });
             });
-        });
+        }
+        getPageOfBbox();
     };
 
     return cardboard;
