45287536432c413311dc5a5f764492d0a3a14392
Matthew Chase Whittemore <mcwhittemore@gmail.com>
2016-11-23 15:21:12 -0500

    clean up mocha output

--- test/geobuf-conversion.test.js
+++ test/geobuf-conversion.test.js
@@ -4,17 +4,20 @@ var fs = require('fs');
 var utils = require('../lib/utils');
 var fixture = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'data', 'numeric-properties.geojson')));
 
-it('[geobuf conversion] cardboard round-trip does not change numeric properties', function(done) {
-    var config = { MAX_GEOMETRY_SIZE: 99999999 };
-    var record = utils(config).toDatabaseRecord(fixture, 'dataset').feature;
-    utils(config).resolveFeatures([record], function(err, collection) {
-        if (err) return done(err);
-        var roundtrip = collection.features[0];
-        assert.equal(
-            roundtrip.properties['@changeset'],
-            fixture.properties['@changeset'],
-            'large numeric feature properties are not adjusted by cardboard roundtrip'
-        );
-        done();
+describe('geobuf-conversion', function() {
+    it('cardboard round-trip does not change numeric properties', function(done) {
+        var config = { MAX_GEOMETRY_SIZE: 99999999 };
+        var record = utils(config).toDatabaseRecord(fixture, 'dataset').feature;
+        utils(config).resolveFeatures([record], function(err, collection) {
+            if (err) return done(err);
+            var roundtrip = collection.features[0];
+            assert.equal(
+                roundtrip.properties['@changeset'],
+                fixture.properties['@changeset'],
+                'large numeric feature properties are not adjusted by cardboard roundtrip'
+            );
+            done();
+        });
     });
 });
+
